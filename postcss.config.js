0b7676877708b492b31b65edd944ef401d7a8b12
Ak Md Ridhauddin <19FTT1151@student.pb.edu.bn>
2021-03-29 11:37:33 +0800

    Trimming file size

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,24 @@
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
 
 module.exports = {
   plugins: [
     tailwindcss,
     autoprefixer,
-  ],
-};
+    require('tailwindcss'),
+    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
+    process.env.NODE_ENV === 'production'
+      ? cssnano({ preset: 'default' })
+      : null,
+    require('autoprefixer'),
+    cssnano({
+      preset: 'default'
+    }),
+    purgecss({
+      content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ]
+}
